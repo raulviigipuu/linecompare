ea95410f33060fbc6c99b9aed7133b1ff8b3cde5
Raul Viigipuu <raul.viigipuu@gmail.com>
2018-12-17 12:02:51 +0200

    basic layout

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log('midagi toimub')
+
+// DOM loaded
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM ready")
+    let compareButton = document.getElementById("compareButton")
+    compareButton.addEventListener("click", handleCompare)
+})
+
+function handleCompare() {
+
+    let leftTextarea = document.getElementById("leftTextarea")
+    let rightTextarea = document.getElementById("rightTextarea")
+
+    let leftLines = leftTextarea.value.split("\n")
+    console.log("LEFT LINE COUNT: " + leftLines.length)
+
+    console.log("LEFT: " + leftTextarea.value)
+    console.log("RIGHT: " + rightTextarea.value)
+}
