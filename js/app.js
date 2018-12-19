98554657c31f142a33f09901dead385c2babd903
Raul Viigipuu <raul@viigipuu.ee>
2018-12-19 20:46:31 +0200

    blah, sleep now

--- js/app.js
+++ js/app.js
@@ -6,15 +6,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
     compareButton.addEventListener("click", handleCompare)
 })
 
+// TODO:
+// Osaliselt kattuvad read
+// Mitu korda esinevad read
+
 function handleCompare() {
 
     let leftTextarea = document.getElementById("leftTextarea")
     let rightTextarea = document.getElementById("rightTextarea")
 
-    let leftLines = leftTextarea.value.split("\n")
-    let rightLines = rightTextarea.value.split("\n")
+    let leftLines = leftTextarea.value.split("\n").filter((line) => line.trim().length > 0)
+    let rightLines = rightTextarea.value.split("\n").filter((line) => line.trim().length > 0)
     console.log("LEFT LINE COUNT: " + leftLines.length)
     console.log("RIGHT LINE COUNT: " + rightLines.length)
+    let commonLines = []
+    let onlyLeft = []
+    let onlyRight = []
+    let count = 0
+    for(let i = 0; i < leftLines.length; i++) {
+        count = 0
+        for(let j = 0; j < rightLines.length; j++) {
 
-    
+            if(leftLines[i] === rightLines[j]) {
+                count++
+                console.log(leftLines[i])
+                commonLines.push(leftLines[i])
+            }
+            
+        }
+        console.log("Element found " + count + " times.")
+    }
+
+    console.log("COMMON LINES: " + commonLines.length)
+    commonLines.forEach((line) => {
+        console.log(line)
+    })
 }
