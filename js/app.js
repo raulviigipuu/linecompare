5a1db711b7d7a0648596620b04c53a47f131d3ba
Raul Viigipuu <raul@viigipuu.ee>
2019-02-09 20:29:19 +0200

    refactor

--- js/app.js
+++ js/app.js
@@ -26,30 +26,181 @@ let DATA = {
 }
 
 let ELEM = {
-    button: {
+    b: { // buttons
         compare: null,
         contains: null,
         clear: null
     },
-    textarea: {
+    ta: { // textareas
         left: null,
         right: null
     },
-    checkbox: {
+    cb: { // checkboxes
         leftSlashReplace: null,
         rightSlashReplace: null
     }
 }
 
-let compareB = null
-let containsB = null
-let clearB = null
-let leftTA = null
-let rightTA = null
-let leftSlashReplaceCB = null
-let rightSlashReplaceCB = null
+class Element {
 
+    static hide(elemId) {
+        let elem = document.getElementById(elemId)
+        if( ! elem.classList.contains('d-none')) {
+            elem.classList.add('d-none')
+        }
+    }
 
+    static show(elemId) {
+        let elem = document.getElementById(elemId)
+        if(elem.classList.contains('d-none')) {
+            elem.classList.remove('d-none')
+        }
+    }
+}
+
+class Handler {
+ 
+    static compare() {
+
+        Cleaner.cleanResults()
+        Element.show(ID.comparison.resultsTable)
+    
+        const linesAll = getLines()
+        const leftLines = linesAll.left
+        const rightLines = linesAll.right
+    
+        let count = 0
+        for (let i = 0; i < leftLines.length; i++) {
+    
+            if (countMatches(DATA.comparison.results.common, leftLines[i]) > 0) {
+                continue
+            }
+    
+            count = 0
+            for (let j = 0; j < rightLines.length; j++) {
+    
+                if (leftLines[i] === rightLines[j]) {
+                    count++
+                }
+            }
+            // Match found!
+            if (count != 0) {
+                DATA.comparison.results.common.push(leftLines[i])
+                let originCount = countMatches(leftLines, leftLines[i])
+                addCommonRow(ID.comparison.results.common, {
+                    text: leftLines[i],
+                    originCount: originCount,
+                    matchCount: count
+                })
+            }
+            // No match, add to left lines table
+            else {
+                if (countMatches(DATA.comparison.results.left, leftLines[i]) > 0) {
+                    continue
+                }
+                DATA.comparison.results.left.push(leftLines[i])
+                addRow(ID.comparison.results.left, {
+                    text: leftLines[i],
+                    count: countMatches(leftLines, leftLines[i])
+                })
+            }
+        }
+    
+        // Check the right lines
+        for (let i = 0; i < rightLines.length; i++) {
+            count = 0
+            for (let j = 0; j < leftLines.length; j++) {
+    
+                if (rightLines[i] === leftLines[j]) {
+                    count++
+                }
+            }
+            // Match found
+            if (count == 0) {
+                if (countMatches(DATA.comparison.results.right, rightLines[i])) {
+                    continue
+                }
+                DATA.comparison.results.right.push(rightLines[i])
+                addRow(ID.comparison.results.right, {
+                    text: rightLines[i],
+                    count: countMatches(rightLines, rightLines[i])
+                })
+            }
+        }
+    }
+
+    static contains() {
+
+        Cleaner.cleanResults()
+        Element.show(ID.contains.resultsTable)
+    
+        const linesAll = getLines()
+        const searchTerms = linesAll.left
+        const lines = linesAll.right
+
+        for (let i = 0; i < searchTerms.length; i++) {
+    
+            let searchTerm = searchTerms[i]
+            let results = search(searchTerm, lines)
+    
+            addContainsRow(ID.contains.results, {
+                nr: i + 1,
+                text: searchTerms[i],
+                count: results.count,
+                lines: results.lines
+            })
+        }
+    }
+
+    static clear() {
+
+        Cleaner.cleanInput()
+        Cleaner.cleanResults()
+    }
+}
+
+class Cleaner {
+
+    static cleanResults() {
+
+        this.clearContent([
+            ID.comparison.results.common, 
+            ID.comparison.results.left, 
+            ID.comparison.results.right,
+            ID.contains.results
+        ])
+
+        DATA.comparison.results.common = []
+        DATA.comparison.results.left = []
+        DATA.comparison.results.right = []
+
+        Element.hide(ID.comparison.resultsTable)
+        Element.hide(ID.contains.resultsTable)
+    }
+
+    static cleanInput() {
+
+        ELEM.ta.left.value = ''
+        ELEM.ta.right.value = ''
+    }
+
+    static deleteDOMContent(elemId) {
+        const elem = document.getElementById(elemId)
+        while (elem.firstChild) {
+            elem.removeChild(elem.firstChild);
+        }
+    }
+
+    static clearContent(idArr) {
+
+        idArr.forEach(e => this.deleteDOMContent(e))
+    }
+
+    static clearData(dataArr) {
+        
+        dataArr.forEach(e => e = [])
+    }
+}
 
 // DOM loaded
 document.addEventListener("DOMContentLoaded", function (event) {
@@ -57,117 +208,37 @@ document.addEventListener("DOMContentLoaded", function (event) {
     console.log("DOM ready")
 
     // Elements
-    compareB = document.getElementById("compareButton")
-    containsB = document.getElementById("containsButton")
-    clearB = document.getElementById("clearButton")
-    leftTA = document.getElementById("leftTextarea")
-    rightTA = document.getElementById("rightTextarea")
-    leftSlashReplaceCB = document.getElementById("leftReplaceBackslashes")
-    rightSlashReplaceCB = document.getElementById("rightReplaceBackslashes")
+    ELEM.b.compare = document.getElementById("compareButton")
+    ELEM.b.contains = document.getElementById("containsButton")
+    ELEM.b.clear = document.getElementById("clearButton")
+    ELEM.ta.left = document.getElementById("leftTextarea")
+    ELEM.ta.right = document.getElementById("rightTextarea")
+    ELEM.cb.leftSlashReplace = document.getElementById("leftReplaceBackslashes")
+    ELEM.cb.rightSlashReplace = document.getElementById("rightReplaceBackslashes")
 
     // Events
-    compareB.addEventListener("click", handleCompare)
-    containsB.addEventListener("click", handleContains)
-    clearB.addEventListener("click", handleCompClear)
+    ELEM.b.compare.addEventListener("click", Handler.compare)
+    ELEM.b.contains.addEventListener("click", Handler.contains)
+    ELEM.b.clear.addEventListener("click", Handler.clear)
 })
 
-function handleCompare() {
-
-    hide(ID.contains.resultsTable)
-    show(ID.comparison.resultsTable) // Make result table visible
-    clearComparisonResults() // Clear previous results
+function getLines() {
 
     // Get the trimmed values and ignore empty lines
-    let leftLines = leftTA.value.split("\n").filter((line) => line.trim().length > 0).map(line => line.trim())
-    let rightLines = rightTA.value.split("\n").filter((line) => line.trim().length > 0).map(line => line.trim())
+    let leftLines = ELEM.ta.left.value.split("\n").filter((line) => line.trim().length > 0).map(line => line.trim())
+    let rightLines = ELEM.ta.right.value.split("\n").filter((line) => line.trim().length > 0).map(line => line.trim())
 
-    if (leftSlashReplaceCB.checked) {
+    if (ELEM.cb.leftSlashReplace.checked) {
         leftLines = leftLines.map(line => line.replace(/\\/g, "/"))
     }
-    if (rightSlashReplaceCB.checked) {
+    if (ELEM.cb.rightSlashReplace.checked) {
         rightLines = rightLines.map(line => line.replace(/\\/g, "/"))
     }
+        
 
-    let count = 0
-
-    for (let i = 0; i < leftLines.length; i++) {
-
-        if (countMatches(DATA.comparison.results.common, leftLines[i]) > 0) {
-            continue
-        }
-
-        count = 0
-        for (let j = 0; j < rightLines.length; j++) {
-
-            if (leftLines[i] === rightLines[j]) {
-                count++
-            }
-        }
-        // Match found!
-        if (count != 0) {
-            DATA.comparison.results.common.push(leftLines[i])
-            let originCount = countMatches(leftLines, leftLines[i])
-            addCommonRow(ID.comparison.results.common, {
-                text: leftLines[i],
-                originCount: originCount,
-                matchCount: count
-            })
-        }
-        // No match, add to left lines table
-        else {
-            if (countMatches(DATA.comparison.results.left, leftLines[i]) > 0) {
-                continue
-            }
-            DATA.comparison.results.left.push(leftLines[i])
-            addRow(ID.comparison.results.left, {
-                text: leftLines[i],
-                count: countMatches(leftLines, leftLines[i])
-            })
-        }
-    }
-
-    // Check the right lines
-    for (let i = 0; i < rightLines.length; i++) {
-        count = 0
-        for (let j = 0; j < leftLines.length; j++) {
-
-            if (rightLines[i] === leftLines[j]) {
-                count++
-            }
-        }
-        // Match found
-        if (count == 0) {
-            if (countMatches(DATA.comparison.results.right, rightLines[i])) {
-                continue
-            }
-            DATA.comparison.results.right.push(rightLines[i])
-            addRow(ID.comparison.results.right, {
-                text: rightLines[i],
-                count: countMatches(rightLines, rightLines[i])
-            })
-        }
-    }
-}
-
-function handleContains() {
-
-    hide(ID.comparison.resultsTable)
-    show(ID.contains.resultsTable)
-    deleteDOMContent(ID.contains.results)
-
-    let searchTerms = leftTA.value.split("\n").filter((line) => line.trim().length > 0).map(line => line.trim())
-    let lines = rightTA.value.split("\n").filter((line) => line.trim().length > 0).map(line => line.trim())
-    for (let i = 0; i < searchTerms.length; i++) {
-
-        let searchTerm = searchTerms[i]
-        let results = search(searchTerm, lines)
-
-        addContainsRow(ID.contains.results, {
-            nr: i + 1,
-            text: searchTerms[i],
-            count: results.count,
-            lines: results.lines
-        })
+    return {
+        left: leftLines,
+        right: rightLines
     }
 }
 
@@ -176,9 +247,9 @@ function search(term, linesArr) {
     let count = 0;
     let lines = []
     // Counts how many lines has term string in them
-    for(let i = 0; i < linesArr.length; i++) {
-        
-        if(linesArr[i].indexOf(term) != -1) {
+    for (let i = 0; i < linesArr.length; i++) {
+
+        if (linesArr[i].indexOf(term) != -1) {
             count++;
             lines.push({
                 lineNr: i + 1,
@@ -187,19 +258,18 @@ function search(term, linesArr) {
         }
     }
 
-    return { count: count, lines: lines}
+    return { count: count, lines: lines }
 }
 
 function highlightTerms(term, line) {
 
-    // <label class='font-weight-bold'>
     let result = ""
     let arr = line.split(term)
 
-    for(let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
 
         // Check if not last element
-        if(i != arr.length - 1) {
+        if (i != arr.length - 1) {
             result += arr[i] + '<label class="font-weight-bold">' + term + '</label>'
         } else {
             result += arr[i]
@@ -207,45 +277,6 @@ function highlightTerms(term, line) {
     }
 
     return result
-}
-
-function handleCompClear() {
-
-    leftTA.value = ''
-    rightTA.value = ''
-    clearComparisonResults()
-    hide(ID.comparison.resultsTable)
-}
-
-function handleContainsClear() {
-
-    leftTA.value = ''
-    rightTA.value = ''
-    clearContainsResults()
-    hide(ID.contains.resultsTable)
-}
-
-function show(elemId) {
-    document.getElementById(elemId).classList.remove('d-none')
-}
-
-function hide(elemId) {
-    document.getElementById(elemId).classList.add('d-none')
-}
-
-// TODO: unify clear - id array for dom delete + data section 
-function clearContainsResults() {
-    console.log("Doing nothing")
-}
-
-function clearComparisonResults() {
-
-    deleteDOMContent(ID.comparison.results.common)
-    deleteDOMContent(ID.comparison.results.left)
-    deleteDOMContent(ID.comparison.results.right)
-    DATA.comparison.results.common = []
-    DATA.comparison.results.left = []
-    DATA.comparison.results.right = []
 }
 
 function countMatches(arr, elem) {
@@ -257,13 +288,6 @@ function countMatches(arr, elem) {
         }
     }
     return count
-}
-
-function deleteDOMContent(parent) {
-    const elem = document.getElementById(parent)
-    while (elem.firstChild) {
-        elem.removeChild(elem.firstChild);
-    }
 }
 
 function addContainsRow(tableId, line) {
@@ -282,8 +306,8 @@ function addContainsRow(tableId, line) {
 function addContainsRowLines(lines) {
 
     let htmlStr = ""
-    for(let i = 0; i < lines.length; i++) {
-        if(htmlStr.length > 0) {
+    for (let i = 0; i < lines.length; i++) {
+        if (htmlStr.length > 0) {
             htmlStr += "<br>"
         }
         htmlStr += `${lines[i].lineNr}. `
